903a1a8e633ab67e8452fcd81f2330bee5c2ff8e
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2023-02-13 07:52:11 -0300

    Add files via upload

---
+++ BDII_IFSP/loja.sql
@@ -0,0 +1,13 @@
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
+
+
+
+CREATE TABLE CD (
+Codigo INTEGER PRIMARY KEY,
+Nome VARCHAR(50),
+Musico VARCHAR(30),
+Genero VARCHAR(30),
+Preco INTEGER
+)
+

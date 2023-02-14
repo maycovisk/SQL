569a5cb97a3ab783985e6eb6dcc1ff628a88d4b6
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2023-02-13 22:34:14 -0300

    Add files via upload

--- BrModelo/SQLQueryAula09.sql
+++ BrModelo/SQLQueryAula09.sql
@@ -34,6 +34,26 @@ GO
 INSERT INTO FUNCIONARIOS VALUES (2, 'Pedro Pereira', 'M', '25/05/2015', 990.00)
 GO
 
+INSERT INTO FUNCIONARIOS VALUES (3, 'Maria Cristina', 'F', '10/09/2015', 1200.00)
+INSERT INTO FUNCIONARIOS VALUES (4, 'Antônio Carlos', 'M', '15/05/2015', 990.00)
+GO
+
+INSERT INTO FUNCIONARIOS VALUES
+	(5, 'Marcelo Augusto', 'M', '09/12/2017', 1900.00),
+	(6, 'Pedro Silva', 'M', '15/11/2015', 1050.00),
+	(7, 'Mônica da Silva', 'F', '12/10/2014', 3000.00),
+	(8, 'Tiago Lima', 'M', '10/05/2016', 1350.50)
+GO
+
 --Exibe todos os dados da tabela FUNCIONARIOS
 SELECT * FROM FUNCIONARIOS
+GO
+
+--Exibe algumas informações dos funcionarios,
+--utilizando aliases (AS) ára açgi,as colunas
+SELECT ID AS 'Código do Funcionário',
+	NOME,
+	Sexo,
+	Salario 'Salário'
+FROM FUNCIONARIOS
 GO
